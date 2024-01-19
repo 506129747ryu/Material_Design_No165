7e54c2e08365c8a25c9735d49f240e3bac3ca69a
506129747ryu <506129747ryu@gmail.com>
2024-01-19 16:23:12 +0900

    library add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,9 @@ android {
 }
 
 dependencies {
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
